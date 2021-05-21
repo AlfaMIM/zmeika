634d37326317c01451e3b81247c8102b598d3b36
opilane <opilane@HP-CZC6297B9K>
2021-05-21 12:20:56 +0300

    class Walls class Point class Snake Class FoodFactory

--- Program.cs
+++ Program.cs
@@ -9,9 +9,13 @@ namespace zmeika
     {
         static readonly int x = 80;
         static readonly int y = 26;
-
+        static Walls walls;
+        static FoodFactory foodFactory;
         static void Main()
         {
+            foodFactory = new FoodFactory(x, y, '@');
+            foodFactory.CreateFood();
+            walls = new Walls(x, y, '#');
             Console.SetWindowSize(x + 1, y + 1);
             Console.SetBufferSize(x + 1, y + 1);
             Console.CursorVisible = false;
@@ -60,14 +64,95 @@ namespace zmeika
 
             private void DrawHorizontal(int x, int y)
             {
-                for (int i = 0; < x; i++)
+                for (int i = 0; i < x; i++)
                 {
                     Point p = (i, y, ch);
                     p.Draw();
-                    wall.Add(p)
+                    wall.Add(p);
                 }
+
             }
             private void DrawVertical(int x, int y)
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    Point p = (x, i, ch);
+                    p.Draw();
+                    wall.Add(p);
+                }
+            }
+        }
+
+        class FoodFactory
+        {
+            int x;
+            int y;
+            char ch;
+            public Point food { get; private set; }
+
+            Random random = new Random();
+
+            public FoodFactory(int x, int y, char ch)
+            {
+                this.x = x;
+                this.y = y;
+                this.ch = ch;
+            }
+            
+            public void CreateFood()
+            {
+                food = (random.Next(2, x - 2), random.Next(2, y - 2), ch);
+                food.Draw();
+            }
+        }
+
+        enum Direction
+        {
+            LEFT,
+            RIGHT,
+            UP,
+            DOWN
+        }
+        class Snake
+        {
+            private List<Point> snake;
+            private Direction direction;
+            private int step = 1;
+            private Point tail;
+            private Point head;
+            bool rotate = true;
+            public Snake(int x, int y, int lenght)
+            {
+                direction = Direction.RIGHT;
+                snake = new List<Point>();
+                for (int i = x - lenght; i < x; i++)
+                {
+                    Point p = (i, y, '*');
+                    snake.Add(p);
+                    p.Draw();
+                }
+            }
+            public Point GetNextPoint()
+            {
+                Point p = GetHead();
+                switch (direction)
+                {
+                    case Direction.LEFT:
+                        p.x -= step;
+                        break;
+                    case Direction.RIGHT:
+                        p.x += step;
+                        break;
+                    case Direction.UP:
+                        p.y -= step;
+                        break;
+                    case Direction.DOWN:
+                        p.y += step;
+                        break;
+                }
+                return p;
+            }
+            public void Rotation(ConsoleKey key)
             {
 
             }
